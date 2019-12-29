be842287186be612b09db49ef5dd439740e35972
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2019-12-29 11:15:11 -0300

    Criando Conexão com o banco, e exportando ela

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// Conectando ao banco
+const db = require('knex') ({
+    client: 'mysql2',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'cadastro'
+    }
+})
+// Dependencias
+const dependencies = {
+    db
+}
+
 // Inicializando o APP
 const app = express();
 // Configurando Porta
@@ -17,13 +32,22 @@ app.set('view engine', 'handlebars');
 // Body-Parser
 app.use(bodyParser.urlencoded({extended: 'false'}));
 
-
 // Routes
 const pessoas = require('./routes/pessoas');
 app.use('/pessoas',pessoas(dependencies));
 
-
 // Rotas
 app.get('/', (req, res)=>{
   res.render('home')
-})
+});
+
+
+connection.connect((err)=>{
+    if (err) console.log('Erro ao se conectar com o banco');
+    else{
+        console.log('Conectado ao banco');
+        app.listen(port, ()=> console.log('APP rodando na porta '+port));
+    }
+});
+  
+  
