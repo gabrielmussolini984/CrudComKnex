08ab03137b3690a40b573a9face1810814bc39ae
Gabriel Mussolini <gabrielmussolini@hotmail.com>
2019-12-29 12:13:38 -0300

    Listando Pessoas

--- routes/pessoas.js
+++ routes/pessoas.js
@@ -5,14 +5,14 @@ const pessoasController = require('../controllers/pessoas');
 
 
 //Injetando dependencias
-const pessoasRouter = ({connection}) =>{
+const pessoasRouter = ({db}) =>{
   const router = express.Router();
-  router.get('/', pessoasController.listaPessoas.bind(null, connection));
-  router.get('/delete/:id', pessoasController.deletePessoa.bind(null, connection));
+  router.get('/', pessoasController.listaPessoas.bind(null, db));
+  /*router.get('/delete/:id', pessoasController.deletePessoa.bind(null, connection));
   router.get('/criarPessoaForm', pessoasController.criarPessoaForm);
   router.post('/criarPessoa', pessoasController.criarPessoa.bind(null, connection));
   router.get('/editarPessoaForm/:id', pessoasController.editarPessoaForm.bind(null, connection));
-  router.post('/editarPessoa/:id', pessoasController.editarPessoa.bind(null, connection));
+  router.post('/editarPessoa/:id', pessoasController.editarPessoa.bind(null, connection));*/
   return router;
 };
 
